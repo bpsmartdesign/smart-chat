a4256dc158a257069124c688b6072c6215de43f7
Biya Paul <bpsmartdesign@hotmail.com>
2025-01-18 10:26:34 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,11 +11,6 @@ import {
   getUserConversations,
   storeMessage,
 } from "./utils/chatUtil";
-import {
-  NOTIF_DB,
-  getUserNotifications,
-  storeNotification,
-} from "./utils/notificationUtil";
 
 const app = express();
 const server = createServer(app);
@@ -24,7 +19,6 @@ const io = new SocketIOServer(server);
 const PORT = 3000;
 
 if (!fs.existsSync(CHAT_DB)) fs.writeFileSync(CHAT_DB, JSON.stringify([]));
-if (!fs.existsSync(NOTIF_DB)) fs.writeFileSync(NOTIF_DB, JSON.stringify([]));
 
 app.use(express.static(path.join(__dirname, "../public")));
 io.on("connection", (socket) => {
@@ -56,30 +50,6 @@ io.on("connection", (socket) => {
   );
   socket.on("typing", ({ sender_id, receiver_id, is_typing }) => {
     io.emit("user_typing", { sender_id, receiver_id, is_typing });
-  });
-  //#endregion
-
-  // #region notification
-  socket.on("register_user", (userId: string) => {
-    console.log(`Registering user to room: ${userId}`);
-    socket.join(userId);
-  });
-  socket.on("get_notifications", (user_id: string) => {
-    const notificationList = getUserNotifications(user_id);
-    socket.emit("notification_list", notificationList);
-  });
-  socket.on("send_notification", ({ title, target_user_id, txt }) => {
-    const newNotification: Notification = {
-      id: uuidv4(),
-      target: "new_msg",
-      target_user_id,
-      title,
-      txt,
-      date: new Date().toISOString(),
-    };
-
-    storeNotification(newNotification);
-    io.to(target_user_id).emit("receive_notification", newNotification);
   });
   //#endregion
 
