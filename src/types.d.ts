2518bc2d7b368ffc89246f3ab14a934ef7b1af85
Biya Paul <bpsmartdesign@hotmail.com>
2025-01-18 11:08:37 +0100

    hot: add conversation_id to handle rooms

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,10 +2,12 @@ export interface Message {
   id: string;
   sender_id: string;
   receiver_id: string;
+  conversation_id: string;
   message: string;
   date: string;
   traveling_date?: string;
 }
+
 export interface Notification {
   id: string;
   target_user_id: string;
